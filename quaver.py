1d805f4f18fd7548a97df682ec67a565fcee358e
Anthony Chavez <anthony@anthonychavez.xyz>
2021-10-18 23:59:08 -0500

    feat: add play command with partial functionality
    The play command has partial functionality, because audio isn't
    played when it's issued. It merely offers error checking, and connects
    the bot to voice channels.

--- quaver.py
+++ quaver.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 from os import getenv
 from discord.ext import commands
 
+def already_in_voice_channel(voice_client):
+    return voice_client and voice_client.is_connected()
+
 load_dotenv()
 
 bot = commands.Bot(command_prefix="!q")
@@ -27,5 +30,28 @@ async def help(ctx):
             )
     await ctx.send(embed=embed)
 
+@commands.command()
+async def play(ctx, url=None):
+    """
+    The play command joins a voice channel if the command issuer is in
+    a voice channel. If the command issuer is not in a voice channel,
+    an error message is displayed. If the command issuer is in a different
+    voice channel than the bot, the bot moves to join that voice channel.
+    If the command is missing a url, an error is displayed.
+    """
+    if url is None:
+        return await ctx.send("The url is missing, try again!")
+    voice_state = ctx.author.voice
+    if voice_state is None:
+        return await ctx.send("You need to be in a voice channel")
+    else:
+        voice_client = discord.utils.get(ctx.bot.voice_clients,
+                                         guild=ctx.guild)
+        if already_in_voice_channel(voice_client):
+            await voice_client.move_to(voice_state.channel)
+        else:
+            await voice_state.channel.connect()
+
+bot.add_command(play)
 
 bot.run(getenv("APP_TOKEN"))
